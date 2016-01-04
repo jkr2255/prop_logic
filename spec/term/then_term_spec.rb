b70dd6a1a55106b2b0c565cb95d45825ca4430bb
Jkr2255 <jkr2255@users.noreply.github.com>
2016-01-05 08:30:55 +0900

    add ThenTerm spec

---
+++ spec/term/then_term_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe PropLogic::ThenTerm do
+  let(:a){PropLogic::Variable.new('a')}
+  let(:b){PropLogic::Variable.new('b')}
+
+  it 'cannot be instantiated directly' do
+    expect{PropLogic::AndTerm.new}.to raise_error(NoMethodError)
+  end
+
+  it 'is not an nnf term' do
+    expect(a >> b).not_to be_nnf
+  end
+  
+  it 'is equivalent to !a | b' do
+    expect(a >> b).to be_equiv(!a | b)
+  end
+end
